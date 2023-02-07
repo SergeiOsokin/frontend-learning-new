5ff9f462c4ab3ba73a94b4c25239e2c1a9a5cd4e
Sergei Osokin <sergei.osokin.rf@mail.ru>
2023-02-07 20:16:25 +0300

    Footer, css, message

--- src/hooks/message.hook.js
+++ src/hooks/message.hook.js
@@ -6,9 +6,9 @@ import { useCallback } from 'react';
 export const useMessage = () => {
     // console.log('sdf')
     return useCallback(text => {
-        // if (window.M && text) {
-        //     window.M.toast({html: text, classes: 'message'})
-        // }
-        <div className='message'>{text}</div>
+        if (window.M && text) {
+            window.M.toast({html: text, classes: 'message'})
+        }
+        // <div className='message'>{text}</div>
     }, []);
 }
