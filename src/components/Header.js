5bcd4de5d1728ce51b411351e22dc524d13bfc84
Sergei Osokin <sergei.osokin.rf@mail.ru>
2025-01-29 21:00:20 +0300

    Change header main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,11 @@ import React, { useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
+import logoWhite from '../../src/img/logo_white.svg';
+import iconUserWhite from '../../src/img/Icon_enter_white.svg';
+
+// переделать меню просто на a
+
 export const Header = () => {
     const { authorization, logout } = useContext(AuthContext); // получаем контекст в объекте auth
     const history = useHistory();
@@ -15,7 +20,19 @@ export const Header = () => {
         <>
             <header className="header">
 
-                <nav className="header__nav">
+                <img className="header__logo header__logo_white" src={logoWhite} alt="Логотип белый"></img>
+
+                <nav class="header__menu">
+                    {/* {!authorization && <li className="header__enter nav__item-li"><img className="header__logo-white" alt="Символ пользователя"></img><Link className="nav__item-a nav__item-a_enter" to="/authorization">Войти</Link></li>} */}
+                    {/* {!authorization && <li className="header__registration nav__item-li"><Link className="nav__item-a nav__item-a_registration" to="/registration">Зарегистрироваться</Link></li>} */}
+                    {!authorization && <div class="header__menu-in">
+                        <img className="header__user-white" src={iconUserWhite} alt="Символ пользователя"></img>
+                        <Link className="header__menu-enter" to="/authorization">Войти</Link>
+                    </div>}
+                    {!authorization && <Link className="header__menu-registration" to="/registration">Зарегистрироваться</Link>}
+                </nav>
+
+                {/* <nav className="header__nav">
                     
                     {authorization && <li className="nav__item-li"><Link className="nav__item-a" to="/">Главная</Link></li>}
 
@@ -51,7 +68,7 @@ export const Header = () => {
                         {!authorization && <li className="nav__item-li"><Link className="nav__item-a" to="/registration">Регистрация</Link></li>}
                         {authorization && <li className="nav__item-li"><Link className="nav__item-a" to="/authorization" onClick={logoutHandler}>Выйти</Link></li>}
                     </ul>
-                </nav>
+                </nav> */}
             </header>
         </>
     )
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -9,40 +9,41 @@ import React from 'react';
 export const MainPage = () => {
 
     return (
-        <main className="main-page commonClass">
-            <section className="title-section">
-                <h1 className='title-section__title'>То, что ты учишь, всегда с тобой.</h1>
-                <p className="title-section__paragraph">Сайт позволяет тебе создавать твою собственную библиотеку грамматических правил, определений,
-                    слов, которые тебе хотелось бы знать на иностранном языке, примеров, которые нужны именно тебе и которые будут понятны для тебя.
-                    То, что ты хочешь знать, всегда с тобой.
-                </p>
-            </section>
+        <main></main>
+        // <main className="main-page commonClass">
+        //     <section className="title-section">
+        //         <h1 className='title-section__title'>То, что ты учишь, всегда с тобой.</h1>
+        //         <p className="title-section__paragraph">Сайт позволяет тебе создавать твою собственную библиотеку грамматических правил, определений,
+        //             слов, которые тебе хотелось бы знать на иностранном языке, примеров, которые нужны именно тебе и которые будут понятны для тебя.
+        //             То, что ты хочешь знать, всегда с тобой.
+        //         </p>
+        //     </section>
 
-            <section className="instrustion-section">
-                <h2 className='instrustion-section__title'>Так просто учиться:</h2>
-                <div className='instrustion-section__items'>
-                    <div className='item'>
-                        <h3 className='item__title'>Регистрируйся</h3>
-                        <p className='item__paragraph'>Никаких рассылок, только создание аккаунта.</p>
-                    </div >
-                    <div className='item'>
-                        <h3 className='item__title'>Создавай правила</h3>
-                        <p className='item__paragraph'>Добавляй название темы, краткое описание правила и примеры.</p>
-                    </div>
-                    <div className='item'>
-                        <h3 className='item__title'>Добавляй категории</h3>
-                        <p className='item__paragraph'>Можешь разделять изучаемые слова по категориям или сделать одну - общую</p>
-                    </div>
-                    <div className='item'>
-                        <h3 className='item__title'>Добавляй слова</h3>
-                        <p className='item__paragraph'>Добавляй слова или целые выражения</p>
-                    </div>
-                    <div className='item'>
-                        <h3 className='item__title'>Повторяй</h3>
-                        <p className='item__paragraph'>Повторяй добавленные слова и запоминай их все лучше</p>
-                    </div>
-                </div>
-            </section>
-        </main>
+        //     <section className="instrustion-section">
+        //         <h2 className='instrustion-section__title'>Так просто учиться:</h2>
+        //         <div className='instrustion-section__items'>
+        //             <div className='item'>
+        //                 <h3 className='item__title'>Регистрируйся</h3>
+        //                 <p className='item__paragraph'>Никаких рассылок, только создание аккаунта.</p>
+        //             </div >
+        //             <div className='item'>
+        //                 <h3 className='item__title'>Создавай правила</h3>
+        //                 <p className='item__paragraph'>Добавляй название темы, краткое описание правила и примеры.</p>
+        //             </div>
+        //             <div className='item'>
+        //                 <h3 className='item__title'>Добавляй категории</h3>
+        //                 <p className='item__paragraph'>Можешь разделять изучаемые слова по категориям или сделать одну - общую</p>
+        //             </div>
+        //             <div className='item'>
+        //                 <h3 className='item__title'>Добавляй слова</h3>
+        //                 <p className='item__paragraph'>Добавляй слова или целые выражения</p>
+        //             </div>
+        //             <div className='item'>
+        //                 <h3 className='item__title'>Повторяй</h3>
+        //                 <p className='item__paragraph'>Повторяй добавленные слова и запоминай их все лучше</p>
+        //             </div>
+        //         </div>
+        //     </section>
+        // </main>
     )
 }
